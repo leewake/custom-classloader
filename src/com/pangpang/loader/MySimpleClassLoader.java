333ec54c381edbb4728b6210fced8ecea5ce6739
lijingwei <lijingwei@lvmama.com>
2017-08-21 16:59:56 +0800

    add getProperty method

--- src/com/pangpang/loader/MySimpleClassLoader.java
+++ src/com/pangpang/loader/MySimpleClassLoader.java
@@ -19,6 +19,7 @@ public class MySimpleClassLoader extends ClassLoader{
 	protected Class<?> findClass(String name) throws ClassNotFoundException {
 
 		byte[] value = CommonFindClass.commonFindClass(byteCode_Path, name);
+		System.out.println("正在调用自定义类加载器");
 
 		//将byte数组转化为一个类的Class对象实例
 		return defineClass(null, value, 0, value.length);
@@ -26,7 +27,10 @@ public class MySimpleClassLoader extends ClassLoader{
 	}
 	//测试
 	public static void main(String[] args) throws ClassNotFoundException {
-		
+
+		System.out.println(System.getProperty("sun.boot.class.path"));
+		System.out.println(System.getProperty("java.ext.dirs"));
+
 		MySimpleClassLoader loader = new MySimpleClassLoader("E:\\H2\\gitTest\\");
 		System.out.println("当前类的父类加载器"+loader.getParent().getClass().getName());
 		System.out.println("加载目标类的类加载器"+loader.loadClass("LoggerAdvice").getClassLoader().getClass().getName());
